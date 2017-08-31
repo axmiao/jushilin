abf68ae70fba068fafb48719bcf5807d3ee079dd
axmiao <18610834353@163.com>
2017-08-31 14:53:06 +0800

    1.0

--- src/js/index.js
+++ src/js/index.js
@@ -48,4 +48,47 @@ $(function(){
 		$(this).addClass('cur').siblings('.sele').removeClass('cur');
 	});
 
-})
+	// 求福弹框；
+	$('.sendbtn').on('click',function(){
+		$('.mask-all,.alert-write').show();
+	})
+	$('.over').on('click',function(){
+		if($('#words').val() !== ''){
+			$('.alert-write').hide();
+			$('.alert-pay').show();
+			$('#words').val('');
+		}else{
+			alert('求福内容不能为空');
+		}
+	});
+	$('.close-pay').on('click',function(){
+		$('.alert-pay,.mask-all').hide();
+	});
+	$('.close-write').on('click',function(){
+		$(".alert-write,.mask-all").hide();
+	})
+
+	$dataLuck = [{
+		'word':'三生三世，十里桃花'
+	},{
+		'word':'愿一世长安，晓梦不烬'
+	},{
+		'word':'愿得一心人，白首不相离'
+	},{
+		'word':'愿人生只如初见，补事秋风悲画扇'
+	},{
+		'word':'时光不老，我们不散'
+	},{
+		'word':'若爱只是过往，从此不见不念'
+	}]
+
+	$('.box-list li').on('click',function(){
+		var x = $(this).index();
+		$(".triange").animate({left:( x * 240 + 20)+'px'})
+		for( var i = 0; i < $dataLuck.length; i++ ){
+			$(".card li").eq(i).find('.info').text($dataLuck[i].word);
+		}
+	})
+
+
+})	
